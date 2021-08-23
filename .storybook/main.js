09d7c9a45b54a10dc10c42ed6b982e02e4f49682
Ayoub El Hayat <ayoubelhayat@gmail.com>
2021-08-23 20:35:55 +0100

    feat: create the task component and its stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/app/components/**/*.stories.ts"
   ],
   "addons": [
     "@storybook/addon-links",
